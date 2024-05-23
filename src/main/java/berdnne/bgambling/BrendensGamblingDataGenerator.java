7e999651b132bd4ae3f505bc82b5cf4e9002b752
berdnne <90113754+berdnne@users.noreply.github.com>
2024-05-22 23:25:19 -0500

    Initial commit

---
+++ src/main/java/berdnne/bgambling/BrendensGamblingDataGenerator.java
@@ -0,0 +1,11 @@
+package berdnne.bgambling;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrendensGamblingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
